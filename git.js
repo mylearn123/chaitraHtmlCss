91d74df6716994b54b57e164f7ab5f2e4e6f7044
mylearn123 <melonchhetri@gmail.com>
2023-07-15 21:50:36 +0545

    Git Info

---
+++ git.js
@@ -0,0 +1,23 @@
+// git remote -v   local system link this file system in online repo  or not
+
+
+/* setup
+1.git init
+2.git remote add origin <git url>
+*/
+
+/* update the code
+1. git status
+    -to check changes in the code
+    - U indicate the untracked
+    - M indicate the Modified
+2. add the code
+     - git add .  // git add all
+     or 
+     - git add <file  path>
+3. git commit -m "feature_name"  // feature name helps to know the message of the file information
+
+4. git push
+
+
+*/
